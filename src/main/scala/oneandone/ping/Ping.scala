41d2eac9fb48f68f637cd4e3c9e69267c76cd7e6
Amel Ajdinović <amel.a.z@gmail.com>
2018-09-24 16:34:04 +0200

    Added Cloud Server API `/ping` and `/ping_auth` endpoints support (tests included).

---
+++ src/main/scala/oneandone/ping/Ping.scala
@@ -0,0 +1,23 @@
+package oneandone.ping
+
+import oneandone.{BooleanCustomSerializer, OneandoneClient, Path}
+import org.json4s.{DefaultFormats, Formats}
+import org.json4s.native.JsonMethods._
+
+
+case class Ping(
+  value: List[String]
+) {}
+
+object Ping extends Path {
+  val serializers                              = List(BooleanCustomSerializer)
+  implicit lazy val serializerFormats: Formats = DefaultFormats ++ serializers
+  override val path: Seq[String]               = Seq("ping")
+
+  def get()(implicit client: OneandoneClient): Ping = {
+    val response = "{\"value\": " + client.get(path) + "}"
+
+    val json     = parse(response).camelizeKeys
+    json.extract[Ping]
+  }
+}
